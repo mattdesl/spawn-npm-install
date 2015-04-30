f4ab23de11b0859a65c32a200ce044a66e06164f
mattdesl <dave.des@gmail.com>
2015-04-29 21:11:03 -0400

    bam!

--- test.js
+++ test.js
@@ -3,62 +3,74 @@ var uninstall = require('./').uninstall
 var test = require('tape')
 var json = require('read-json')
 var path = require('path')
+var concat = require('concat-stream')
 
-test('no deps should install', function(t) {
+test('no deps should install', function (t) {
   var zalgo = true
-  install([], function(err, data) {
-    t.deepEqual(data.packages, [], 'no packages')
-    t.deepEqual(data.tree, [], 'no tree')
+  install([], function (err) {
+    if (err) t.fail(err)
     t.equal(zalgo, false, 'method is async')
   })
   zalgo = false
-  t.plan(3)
+  t.plan(1)
 })
 
-test('should install deps', function(t) {
+test('should fail on error', function (t) {
   t.plan(2)
-  var list = ['through', 'tape', 'quote-stream']
-  install(list, {}, function(err, data) {
+  var list = ['throuaaa(*@gh']
+  install(list, {}, function (err) {
+    t.equal(typeof err.message, 'string', 'err.message')
+    t.notEqual(err, null, 'an error is returned')
+  })
+})
+
+test('should succeed', function (t) {
+  t.plan(1)
+  var list = ['through', 'zalgo']
+  install(list, {}, function (err) {
+    t.equal(err, null, 'no error is returned')
+  })
+})
+
+test('should print some text', function (t) {
+  t.plan(1)
+  install('through', function (err) {
+    if (err) t.fail(err)
+  }).stdout.pipe(concat(function (body) {
+    t.notEqual(body.toString().indexOf('through'), -1, 'downloaded body')
+  }))
+})
+
+test('should --save-dev to package.json', function (t) {
+  t.plan(1)
+  install('quote-stream', {
+    saveDev: true
+  }, function (err) {
+    if (err) t.fail(err)
+    includes(t, 'quote-stream', true)
+  })
+})
+
+test('should uninstall --save-dev', function (t) {
+  t.plan(1)
+  var list = ['quote-stream']
+  uninstall(list, {
+    saveDev: true
+  }, function (err, data) {
     if (err) return t.fail(err)
-
-    var names = data.packages.map(toName)
-    t.deepEqual(names, list)
-    t.ok(data.tree.length !== 0, 'has some entries')
+    includes(t, 'quote-stream', false)
   })
 })
 
-test('should install --save-dev deps', function(t) {
-  t.plan(2)
-  var list = ['zalgo']
-  install(list, { saveDev: true }, function(err, data) {
-    var names = data.packages.map(toName)
-    t.deepEqual(names, ['zalgo'], 'installed --save-dev zalgo')
-    includes(t, 'zalgo', true)
-  })
-})
-
-// test('should uninstall --save-dev', function(t) {
-//   t.plan(2)
-//   var list = ['zalgo']
-//   uninstall(list, {}, function(err, data) {
-//     if (err) return t.fail(err)
-//     t.deepEqual(data, ['zalgo'], 'uninstalls zalgo')
-//     includes(t, 'zalgo', false)
-//   })
-// })
-
-function toName(pkg) {
-  return pkg.split('@')[0]
-}
-
-function includes(t, name, expected) {
-  json(path.join(__dirname, 'package.json'), function(err, data) {
+function includes (t, name, expected) {
+  json(path.join(__dirname, 'package.json'), function (err, data) {
     if (err) t.fail(err)
 
     var idx = Object.keys(data.devDependencies).indexOf(name)
-    if (expected)
+    if (expected) {
       t.notEqual(idx, -1, 'has dep')
-    else
+    } else {
       t.equal(idx, -1, 'does not have dep')
+    }
   })
 }
